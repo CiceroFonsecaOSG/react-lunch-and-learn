38e4f2652f5484b96e48689c17a5a3c25abd9682
Cicero Fonseca <cicero@osg.ca>
2022-03-23 16:42:48 -0400

    Create portal and display modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Customizer from "./Customizer/Customizer";
+import Portal from "../Portal";
 
 const Card = ({ content }) => {
   const [showModal, setShowModal] = useState(false);
@@ -44,6 +46,21 @@ const Card = ({ content }) => {
           <button className="card__btn btn" onClick={() => toggleModal()}>
             Buy Course
           </button>
+          {showModal && (
+            <Portal>
+              <div id="modal">
+                <div>
+                  <Customizer />
+                  <button
+                    className="card__btn btn"
+                    onClick={() => toggleModal()}
+                  >
+                    Done
+                  </button>
+                </div>
+              </div>
+            </Portal>
+          )}
         </div>
       </div>
     </article>
